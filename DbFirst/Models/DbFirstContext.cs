368cae6a8db6462c75e28310d473997bcfd6a02f
gogagelkhauri <gogagelkhauri1@gmail.com>
2023-06-12 09:30:34 +0400

    Initial

---
+++ DbFirst/Models/DbFirstContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirst.Models;
+
+public partial class DbFirstContext : DbContext
+{
+    public DbFirstContext()
+    {
+    }
+
+    public DbFirstContext(DbContextOptions<DbFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Note> Notes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost;Database=DbFirst;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Note>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Description).HasMaxLength(500);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DbFirst/Program.cs
@@ -0,0 +1,13 @@
+﻿using DbFirst.Models;
+
+var context = new DbFirstContext();
+
+var notes = context.Notes.ToList();
+
+foreach(var note in notes)
+{
+    Console.WriteLine(note.Name);
+}
+
+//commant for connection
+// Scaffold-DbContext "Server=localhost;Database=DbFirst;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -force
